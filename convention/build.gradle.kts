f966dc8d12c09cbc4ff235afb7832941732276b6
Ángel Gómez <raxden.dev@gmail.com>
2025-01-02 12:51:17 +0100

    include serialization plugin to build gradle

--- convention/build.gradle.kts
+++ convention/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
     implementation(libs.kotlin.compose.gradlePlugin)
+    implementation(libs.kotlin.serialization.gradlePlugin)
     implementation(libs.rootcoverage.gradlePlugin)
     implementation(libs.detekt.gradlePlugin)
 }
